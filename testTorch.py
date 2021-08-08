09bc6ff42f1aa42949efde33d00c33b6522341f8
WenRuiCai <844432667@qq.com>
2021-08-08 17:56:31 +0800

    testVSCodeGit

--- testTorch.py
+++ testTorch.py
@@ -1,2 +1,11 @@
-import torch
-torch.cuda.current_device()
+epoch = 0
+iteration = 10
+losses = [2.44161]
+
+total = 164646
+eta_str = 1864648
+elapsed = 46464864
+loss_labels = [6456, 456]
+
+print(('[%3d] %7d ||' + (' %s: %.3f |' * len(losses)) + ' T: %.3f || ETA: %s || timer: %.3f')
+      % tuple([epoch, iteration] + loss_labels + [total, eta_str, elapsed]), flush=True)
